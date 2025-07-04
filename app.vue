e08af7239dbbd278091061a78d29e6af556e44f7
María Fernández <matefe1706@gmail.com>
2025-07-04 14:26:53 +0200

    feat(pages): wrap NuxtPage in Transition for fade effect

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage />
+      <Transition name="fade-pages" mode="default" appear>
+        <NuxtPage :key="$route.path" />
+      </Transition>
     </NuxtLayout>
   </div>
 </template>
