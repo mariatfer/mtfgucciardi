54a65182ec2e66b0926166b38ec378236db4d9c4
María Fernández <matefe1706@gmail.com>
2025-07-04 19:10:02 +0200

    feat(pages): implement About Me page layout with responsive styles

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,7 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div></div>
+  <div class="about">
+    <article class="about__container">
+      <section>
+        <ViewsAboutProfileCard />
+      </section>
+    </article>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.about {
+  margin: var(--s-margin-blocks) 0;
+  padding: 0 var(--s-padding-lateral);
+  @include responsive {
+    margin: var(--s-margin-blocks-mobile) 0;
+    padding: 0 var(--s-padding-lateral-mobile);
+  }
+
+  &__container {
+    @include flex(row, center, space-between);
+    gap: 2rem;
+    @include responsive {
+      @include flex(column, center, space-between);
+      gap: 1rem;
+    }
+  }
+}
+</style>
