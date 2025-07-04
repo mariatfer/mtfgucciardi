2873a6fc26530d18d6983d5f5122ab4b7e6b773c
María Fernández <matefe1706@gmail.com>
2025-07-04 19:09:04 +0200

    feat(components): add content of ProfileCard component

---
+++ mocks/about.ts
@@ -0,0 +1,13 @@
+export const ProfileCard = {
+  title: "About Me",
+  description: [
+    {
+      id: "1",
+      text: "I'm María, a Full Stack Developer with a passion for crafting dynamic and efficient web applications.",
+    },
+    {
+      id: "2",
+      text: "I specialize in both front-end and back-end development, ensuring a seamless user experience.",
+    },
+  ],
+};
