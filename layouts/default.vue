daff01cde6eb6ec5fa68473f32400f933b3b6d30
María Fernández <matefe1706@gmail.com>
2025-07-06 23:10:22 +0200

    feat(layout): add dynamic background classes for index and default routes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
+  <div :class="$route.name === 'index' ? 'bg-index' : 'bg-default'">
     <UiNavbarMainNavbar />
     <slot />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.bg-index {
+  background-color: var(--c-aquamarine);
+}
+.bg-default {
+  background-color: var(--c-light-aquamarine);
+}
+</style>
