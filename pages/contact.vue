6c530a5a00a4c49fa1cec4ed1550203be37fbcde
María Fernández <matefe1706@gmail.com>
2025-07-08 18:37:57 +0200

    feat(pages): add structure and styles

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div class="contact">
+    <ViewsContactInfoContact />
+    <ViewsContactForm class="contact__form" />
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.contact {
+  @include flex(column, flex-start, flex-start, $gap: 1.5rem);
+  @include responsive {
+    @include flex(column, $gap: 1.5rem);
+  }
+}
+</style>
