d969ba5b6ab407dab65c920aac4c1fa429e040c8
María Fernández <matefe1706@gmail.com>
2025-07-08 18:39:59 +0200

    feat(server): add resend api

---
+++ server/api/send.ts
@@ -0,0 +1,28 @@
+import { Resend } from "resend";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const resend = new Resend(config.RESEND_API_KEY);
+  const body = await readBody(event);
+  // Validar datos básicos
+  if (!body.email || !body.message) {
+    return { status: false, error: "Faltan campos obligatorios." };
+  }
+  try {
+    const data = await resend.emails.send({
+      from: "Mi Portfolio <onboarding@resend.dev>",
+      to: ["mt.fgucciardi@gmail.com"],
+      subject: "Nuevo mensaje de tu portfolio",
+      html: `
+      <p><strong>Email:</strong> ${body.email}</p>
+      <p><strong>Mensaje:</strong></p>
+      <p>${body.message}</p>
+    `,
+    });
+
+    return { status: true, data };
+  } catch (err) {
+    console.error(err);
+    return { status: false, error: "Error enviando el correo." };
+  }
+});
