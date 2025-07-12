3d4e62b0f73bc9ad11e804abcfb0a9179d32b6b8
María Fernández <matefe1706@gmail.com>
2025-07-12 19:11:35 +0200

    feat(pages): add project page structure

---
+++ pages/projects.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { projects } from "@/mocks/projects";
+const { isResponsiveResolution } = useWindowsResize();
+
+const textAlign = computed(() =>
+  isResponsiveResolution.value ? "center" : "left"
+);
+</script>
+
+<template>
+  <div class="projects">
+    <UiTheTitle
+      :text-align="textAlign"
+      color="var(--c-primary)"
+      :shadow="false"
+      :uppercase="true"
+      :bolder="true"
+      >{{ projects.title }}</UiTheTitle
+    >
+    <UiScrollReveal
+      v-for="card in projects.projectCards"
+      :key="card.id"
+      style="width: 100%"
+    >
+      <ViewsProjectsProjectCard :card="card" />
+    </UiScrollReveal>
+  </div>
+</template>
+
+<style lang="scss">
+.projects {
+  margin: var(--s-margin-blocks) 0;
+  padding: 0 var(--s-padding-lateral);
+ 
+  @include responsive {
+    margin: var(--s-margin-blocks-mobile) 0;
+    padding: 0 var(--s-padding-lateral-mobile);
+  }
+}
+</style>
