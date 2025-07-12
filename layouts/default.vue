0356d90f07d16c530973597ca7eb7cdbe0add41e
María Fernández <matefe1706@gmail.com>
2025-07-12 20:03:04 +0200

    fix(components): fix layout styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
-  <div :class="$route.name === 'index' ? 'bg-index' : 'bg-default'">
+  <div
+    class="layout"
+    :class="$route.name === 'index' ? 'bg-index' : 'bg-default'"
+  >
     <UiNavbarMainNavbar />
     <slot />
   </div>
 </template>
 
 <style lang="scss" scoped>
+.layout {
+  padding: 5rem 0 0 0;
+}
 .bg-index {
   background-color: var(--c-aquamarine);
 }
