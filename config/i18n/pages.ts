9e2c11fc7f401f1039b4e6fcc49c486ef4c06b6d
María Fernández <matefe1706@gmail.com>
2025-07-30 17:29:51 +0200

    feat(config): add i18n configuration

---
+++ config/i18n/pages.ts
@@ -0,0 +1,18 @@
+export const pages = {
+  "about-me": {
+    es: "/sobre-mi",
+    en: "/about-me",
+  },
+  projects: {
+    es: "/proyectos",
+    en: "/projects",
+  },
+  contact: {
+    es: "/contacto",
+    en: "/contact",
+  },
+  "thank-you": {
+    es: "/gracias",
+    en: "/thank-you",
+  },
+};
